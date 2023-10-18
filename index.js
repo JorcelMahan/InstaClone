018f259a1686238ccf22144b752046a7959fa99f
JomaFC <johanmfc11@gmail.com>
2023-10-18 06:38:01 -0400

    setup mongodb

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from '@apollo/server/standalone';
 import typeDefs from "./gql/schema.js";
 import resolvers from "./gql/resolver.js";
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDB from "./config/db.js";
+
+connectDB();
 
 const server = new ApolloServer({
     typeDefs,
